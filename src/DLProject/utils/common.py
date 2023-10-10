6d40a73f069184b65e5e27a7bbf4639dd743b18c
Nitin Choudhary <chnitin012@gmail.com>
2023-10-10 15:17:25 +0530

    Fixed the yaml opening file issue

--- src/DLProject/utils/common.py
+++ src/DLProject/utils/common.py
@@ -21,7 +21,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         ConfigBox: ConfigBox containing the YAML content.
     """
     try:
-        with open(path_to_yaml, 'r') as yaml_file:
+        with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
             logger.info(f"YAML file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)

f23c13045d239f94f39e0d655d79042abfb5603e
Nitin Choudhary <chnitin012@gmail.com>
2023-10-10 18:11:52 +0530

    Added prapare base model code

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from DLProject import logger
 from DLProject.pipeline.step_01_data_ingestion import DataIngestionTrainingPipeline
+from DLProject.pipeline.step_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
-try:
-    data_ing = DataIngestionTrainingPipeline()
-    data_ing.main()
-except Exception as e:
-    logger.exception(e)
-    raise e
+class MainClass:
+    
+    def __init__(self):
+        pass
 
+    def callDataIngestion(self):
+        try:
+            data_ing = DataIngestionTrainingPipeline()
+            data_ing.main()
+        except Exception as e:
+            logger.exception(e)
+            raise e
+        
+    def callPrepareBaseModel(self):
+        try:
+            prep_model = PrepareBaseModelTrainingPipeline()
+            prep_model.main()
+
+        except Exception as e:
+            logger.exception(e)
+            raise e
+
+
+if __name__ == "__main__":
+    cls = MainClass()
+    # cls.callDataIngestion()
+    cls.callPrepareBaseModel()

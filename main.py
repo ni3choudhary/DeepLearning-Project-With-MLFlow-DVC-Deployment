28fdeb72abb4d6bf83cc276cb82b6be271f50cc3
Nitin Choudhary <chnitin012@gmail.com>
2023-10-11 15:04:58 +0530

    Added prapare model training code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from DLProject import logger
 from DLProject.pipeline.step_01_data_ingestion import DataIngestionTrainingPipeline
 from DLProject.pipeline.step_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from DLProject.pipeline.step_03_model_training import ModelTrainingPipeline
 
 class MainClass:
     
@@ -9,23 +10,38 @@ class MainClass:
 
     def callDataIngestion(self):
         try:
+            # Create an instance of the DataIngestionTrainingPipeline and call its 'main' method.
             data_ing = DataIngestionTrainingPipeline()
             data_ing.main()
         except Exception as e:
+            # Log any exceptions that occur and re-raise them.
             logger.exception(e)
-            raise e
+            raise e   # Raising the exception further for error handling.
         
     def callPrepareBaseModel(self):
         try:
+            # Create an instance of the PrepareBaseModelTrainingPipeline and call its 'main' method.
             prep_model = PrepareBaseModelTrainingPipeline()
             prep_model.main()
 
         except Exception as e:
+             # Log any exceptions that occur and re-raise them.
+            logger.exception(e)
+            raise e
+        
+    def callModelTraining(self):
+        try:
+            # Create an instance of the ModelTrainingPipeline and call its 'main' method.
+            model_trn_pipeline = ModelTrainingPipeline()
+            model_trn_pipeline.main()
+        except Exception as e:
+            # Log any exceptions that occur and re-raise them.
             logger.exception(e)
             raise e
 
 
 if __name__ == "__main__":
     cls = MainClass()
-    # cls.callDataIngestion()
+    cls.callDataIngestion()
     cls.callPrepareBaseModel()
+    cls.callModelTraining()

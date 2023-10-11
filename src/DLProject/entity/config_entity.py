3410cc1c154b16bcad56c068e4229824a0ae004a
Nitin Choudhary <chnitin012@gmail.com>
2023-10-11 15:00:52 +0530

    Added model training config entity

--- src/DLProject/entity/config_entity.py
+++ src/DLProject/entity/config_entity.py
@@ -21,3 +21,28 @@ class PrepareBaseModelConfig:
     params_include_top: bool
     params_weights: str
     params_classes: int
+
+
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
+    params_validation_split: float
+    params_interpolation: str
+    params_validation_data_generator_subset: str
+    params_training_data_generator_subset: str
+    params_is_validation_data_generator_shuffle: bool
+    params_is_training_data_generator_shuffle: bool
+    params_rotation_range: int
+    params_is_horizontal_flip: bool
+    params_width_shift_range: float 
+    params_height_shift_range: float
+    params_shear_range: float
+    params_zoom_range: float
+    source_dir: Path

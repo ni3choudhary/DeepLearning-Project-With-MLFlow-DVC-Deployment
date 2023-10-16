5c072f4ab2a76a7d43bea59f7b72d17b0e7c53be
ni3choudhary <chnitin012@gmail.com>
2023-10-16 15:20:43 +0530

    updated model training config entity

--- src/DLProject/entity/config_entity.py
+++ src/DLProject/entity/config_entity.py
@@ -27,6 +27,7 @@ class PrepareBaseModelConfig:
 class TrainingConfig:
     root_dir: Path
     trained_model_path: Path
+    copy_trained_model_path: Path
     updated_base_model_path: Path
     training_data: Path
     params_epochs: int
